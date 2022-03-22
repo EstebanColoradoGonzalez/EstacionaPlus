1c50e9b44d9c555bfa9a3ff77768367e33334f77
Esteban <estebancoloradogonzalez@gmail.com>
2022-03-21 23:44:26 -0500

    Se corrigen tests

--- src/test/java/co/edu/uco/estacionaplus/domain/model/ReservedTimeTest.java
+++ src/test/java/co/edu/uco/estacionaplus/domain/model/ReservedTimeTest.java
@@ -9,21 +9,21 @@ public class ReservedTimeTest
     void validateSuccessfulCreation()
     {
 
-        var reservedTime = ReservedTime.create(1, "2", "Horas");
+        var reservedTime = ReservedTime.create(1, 2, "Horas");
 
-        Assertions.assertEquals("2", reservedTime.getValue());
+        Assertions.assertEquals(2, reservedTime.getValue());
         Assertions.assertEquals("Horas", reservedTime.getTypeTime());
     }
 
     @Test
     void validateMissingFields()
     {
-        Assertions.assertEquals("The value of a ReservedTime cannot be empty.",Assertions.assertThrows(IllegalArgumentException.class, () -> ReservedTime.create(1, null, "Horas")).getMessage());
+        Assertions.assertEquals("The type of Time of a ReservedTime cannot be empty.",Assertions.assertThrows(IllegalArgumentException.class, () -> ReservedTime.create(1, 1, null)).getMessage());
     }
 
     @Test
     void ValidateEmptyFields()
     {
-        Assertions.assertEquals("The value of a ReservedTime cannot be empty.", Assertions.assertThrows(IllegalArgumentException.class, () -> ReservedTime.create(1, "", "Horas")).getMessage());
+        Assertions.assertEquals("The type of Time of a ReservedTime cannot be empty.", Assertions.assertThrows(IllegalArgumentException.class, () -> ReservedTime.create(1, 1, "")).getMessage());
     }
 }
